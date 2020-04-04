9d41ac099a2b5d50bd5080c703cf23013c7d844c
Reyes, Oliver <oliver.reyes@venmo.com>
2020-04-04 12:22:07 -0700

    Added button to start MyReactActivity

--- android/app/src/main/java/com/yarn/appissue/MainActivity.java
+++ android/app/src/main/java/com/yarn/appissue/MainActivity.java
@@ -7,14 +7,19 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private final int OVERLAY_PERMISSION_REQ_CODE = 1;
+
+    private Button btnLaunchRN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLaunchRN = findViewById(R.id.btnLaunchRN);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (!Settings.canDrawOverlays(this)) {
@@ -23,6 +28,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
             }
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        btnLaunchRN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchRN();
+            }
+        });
+    }
+
+    private void launchRN() {
+        Intent intent = new Intent(this, MyReactActivity.class);
+        startActivity(intent);
     }
 
     @Override
